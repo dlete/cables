a18f026966636ea39f3d5b5ae302ba80c47ca972
Daniel Lete <dlete@fermin.acme.net>
2011-03-06 11:05:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cables::Application.config.secret_token = 'f79f072f85cc2457e7c29ffa5ea8df8c446e7cef9665249bb388a4e487976ccd7816e1420edfc9d5a993c87cc63ee25b7ea05bc05b56971b22b42dbdb38d002c'
