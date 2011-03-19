2f8e9af16554cc0ff0071983363aeade9e22db53
Daniel Lete <daniel.lete@acme.net>
2011-03-19 15:41:39 +0000

    create chart Total Circuits Per Date and move all charts to the Reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,5 @@
 class ReportsController < ApplicationController
   def summary
-    @providers_total = Provider.all.count
-    @circuits_total = Circuit.all.count
-    @endpoints_total = Endpoint.all.count
-    @circuits_per_provider = ( @circuits_total / @providers_total )
-    @circuits_per_endpoint = ( @circuits_total / @endpoints_total )
   end
 
 end
--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -7,6 +7,10 @@ class Circuit < ActiveRecord::Base
   belongs_to :end_a_medium, :class_name => 'Medium'
   belongs_to :end_b_medium, :class_name => 'Medium'
 
+  def self.total_up_to_a_date(date)
+    where("date(created_at) < ?",date).count
+  end
+
   def self.provider_name
 # this gives me: [BT Ireland, Stracke-Beatty, Pfannerstill-Pollich, Olson-Sipes, Eircom, Dublin City Corporation]
 #    "[" + (Provider.first.id..Provider.last.id).map { |blah| Provider.find(blah).name}.join(", ") + "]"
