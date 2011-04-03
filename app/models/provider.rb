e8142381c944cec7ab2d3e3c5c00d3ee0175cbc9
Daniel Lete <daniel.lete@acme.net>
2011-04-03 03:35:15 +0100

    now Providers as foreign-key of Organizations works

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,14 +1,4 @@
 class Provider < ActiveRecord::Base
-  validates_length_of :name,
-    :minimum => 2,
-    :maximum => 99
-  validates :abbreviation, :length => {
-    :minimum => 2,
-    :maximum => 99
-  }
-
   belongs_to :organization
   has_many :circuits
-  cattr_reader :per_page
-  @@per_page = 13
 end
