4dcce9ff13af65c677e1af103534b9d25ec36333
Daniel Lete <daniel.lete@acme.net>
2011-07-30 08:25:49 +0100

    CircuitLeg has create and destroy completed

--- app/controllers/circuit_legs_controller.rb
+++ app/controllers/circuit_legs_controller.rb
@@ -1,7 +1,25 @@
 class CircuitLegsController < ApplicationController
+
   def create
     @circuit = Circuit.find(params[:circuit_id])
     @circuit_leg = @circuit.circuit_legs.create(params[:circuit_leg])
     redirect_to circuit_path(@circuit)
+  end
+
+  def destroy
+    @circuit = Circuit.find(params[:circuit_id])
+    @circuit_leg = @circuit.circuit_legs.find(params[:id])
+    @circuit_leg.destroy
+    redirect_to circuit_path(@circuit)
+  end
+
+  def edit
+    @circuit = Circuit.find(params[:circuit_id])
+    @circuit_leg = @circuit.circuit_legs.find(params[:id])
+  end
+
+  private 
+  def load_auxiliary_data
+    @links = Link.all
   end
 end
