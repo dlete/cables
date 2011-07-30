b5eb4398daec4f8f46ed88365455dd6dd6ea7251
Daniel Lete <daniel.lete@acme.net>
2011-07-30 06:30:04 +0100

    add CircuitLegs controller, now it works

--- app/controllers/circuit_legs_controller.rb
+++ app/controllers/circuit_legs_controller.rb
@@ -1,7 +1,7 @@
 class CircuitLegsController < ApplicationController
   def create
     @circuit = Circuit.find(params[:circuit_id])
-    @circuit_leg = @circuit.circuit_legs.create(params[:link_id])
+    @circuit_leg = @circuit.circuit_legs.create(params[:circuit_leg])
     redirect_to circuit_path(@circuit)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110730005549) do
+ActiveRecord::Schema.define(:version => 20110730043953) do
 
   create_table "circuit_legs", :force => true do |t|
     t.integer  "circuit_id"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(:version => 20110730005549) do
     t.integer  "circuit_end_b_endpoint_id"
     t.integer  "circuit_end_b_medium_id"
     t.integer  "circuit_medium_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clegs", :force => true do |t|
+    t.integer  "circuit_id"
+    t.integer  "link_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
