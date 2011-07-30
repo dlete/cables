8022f590cbaa23004e92c9963af91e2c604e1c01
Daniel Lete <daniel.lete@acme.net>
2011-07-30 12:40:43 +0100

    finished CircuitLeg MCV, pending to clean up and refractor

--- app/controllers/circuit_legs_controller.rb
+++ app/controllers/circuit_legs_controller.rb
@@ -14,8 +14,20 @@ class CircuitLegsController < ApplicationController
   end
 
   def edit
+    load_auxiliary_data
     @circuit = Circuit.find(params[:circuit_id])
     @circuit_leg = @circuit.circuit_legs.find(params[:id])
+  end
+
+  def update
+    @circuit = Circuit.find(params[:circuit_id])
+    @circuit_leg = CircuitLeg.find(params[:id])
+    if @circuit_leg.update_attributes(params[:circuit_leg])
+      flash[:notice] = "Successfully updated leg."
+      redirect_to circuit_url(@circuit_leg.circuit_id)
+    else
+      render :action => 'edit'
+    end
   end
 
   private 
